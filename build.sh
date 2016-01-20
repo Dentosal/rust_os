e8a8f80a610ac4926821b00c2631d5102b10c658
Dentosal <hannes.karppila@gmail.com>
2016-01-20 19:41:22 +0200

    Build system compiles nowls

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ set -e
 mkdir -p build
 
 # compile bootloader
-nasm src/boot/boot.asm -f bin -o boot.bin
+nasm src/boot/boot.asm -f bin -o build/boot.bin
+
+# compile kernel entry point
+nasm -f elf64 src/entry.asm -o build/entry.o
 
 # compile kernel
 cargo rustc --target x86_64-unknown-linux-gnu -- -Z no-landing-pads
 
 # link
-ld -n --gc-sections -T buildsystem/linker.ld -o kernel.bin target/x86_64-unknown-linux-gnu/debug/librust_os.a
+ld -n --gc-sections -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/x86_64-unknown-linux-gnu/debug/librust_os.a
 
 
 # floppify :] ( or maybe imagify, isofy or harddiskify)

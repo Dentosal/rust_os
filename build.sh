94cd44066b1146f9e3875824f474cb0397f59832
Dentosal <hannes.karppila@gmail.com>
2016-01-20 18:53:58 +0200

    Improve buildsystem

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
-nasm src/boot/boot.asm -f bin -o ../start.img
-# cargo rustc --target x86_64-unknown-linux-gnu -- -Z no-landing-pads
+# prepare for build
+mkdir -p build
+
+# compile bootloader
+nasm src/boot/boot.asm -f bin -o boot.bin
+
+# compile kernel
+cargo rustc --target x86_64-unknown-linux-gnu -- -Z no-landing-pads
+
+# link
+ld -n --gc-sections -T buildsystem/linker.ld -o kernel.bin $(assembly_object_files) target/x86_64-unknown-linux-gnu/debug/libblog_os.a
+
+
+# floppify :] ( or maybe imagify, isofy or harddiskify)

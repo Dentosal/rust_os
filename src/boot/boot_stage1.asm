390f2a1614281bacc4dbf4063b8c4b4fcdeb3c01
Dentosal <hannes.karppila@gmail.com>
2016-01-21 10:24:56 +0200

    Fix binary size of boot_stage1

--- src/boot/boot_stage1.asm
+++ src/boot/boot_stage1.asm
@@ -11,7 +11,4 @@ stage1:
     ;call 0x7e00
     jmp $
 
-times (0x200 - 0x2)-($-$$) db 0
-db 0x55
-db 0xaa
-times (0x000b4000 - 0x200) db 0 ; Fill floppy (Standard 1.44M IBM Floppy)
+times (0x200-($-$$)) db 0 ; fill sector

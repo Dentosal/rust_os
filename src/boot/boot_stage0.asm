d4003d58208ceb327f91ac08427b19e922c659fa
Dentosal <hannes.karppila@gmail.com>
2016-01-24 19:41:41 +0200

    Move stuff to stage1, there is plenty of free space

--- src/boot/boot_stage0.asm
+++ src/boot/boot_stage0.asm
@@ -23,7 +23,7 @@ boot:
     mov dh, 0       ; head
     mov bx, (loadpoint - 0x200)  ; read buffer (now next stage is located at (loadpoint - 0x200) and kernel just after that)
     int 0x13
-    jc error
+    jc load_error
     ; load protected mode GDT and a null IDT (we don't need interrupts)
     cli
     lgdt [gdtr32]
@@ -35,7 +35,7 @@ boot:
     ; far jump to load CS with 32 bit segment
     jmp 0x08:protected_mode
 
-error:
+load_error:
     mov si, .msg
 .loop:
     lodsb
@@ -49,6 +49,15 @@ error:
     .msg db "could not read disk", 0
 
 [BITS 32]
+; Page tables
+%define page_table_section_start 0x00020000
+%define page_table_p4 0x00020000
+%define page_table_p3 0x00021000
+%define page_table_p2 0x00022000
+%define page_table_section_end 0x00023000
+
+
+
 protected_mode:
     ; load all the other segments with 32 bit data segments
     mov eax, 0x10
@@ -60,10 +69,19 @@ protected_mode:
     ; set up stack
     mov esp, 0x7c00 ; stack grows downwards
 
-    call enable_A20
+    ; SCREEN: top left: "00"
+    mov dword [0xb8000], 0x2f302f30
 
-    ; SCREEN: top left: "0 "
-    mov dword [0xb8000], 0x2f202f30
+    call enable_A20
+    call check_long_mode
+    call set_up_page_tables
+    call enable_paging
+    call set_up_SSE
+
+
+    ; SCREEN: top left: "01"
+    mov dword [0xb8000], 0x2f302f31
+
 
     ; jump into stage 1
     call 0x7e00
@@ -82,15 +100,135 @@ enable_A20:
 .done:
     ret
 
+
+; Check for SSE and enable it.
+; http://os.phil-opp.com/set-up-rust.html#enabling-sse
+; http://wiki.osdev.org/SSE
+set_up_SSE:
+    ; check for SSE
+    mov eax, 0x1
+    cpuid
+    test edx, 1<<25
+    jz .SSE_missing
+
+    ; enable SSE
+    mov eax, cr0
+    and ax, 0xFFFB      ; clear coprocessor emulation CR0.EM
+    or ax, 0x2          ; set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9       ; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
+
+    ret
+.SSE_missing:
+    ; error: no SSE: "!S"
+    mov al, '!'
+    mov ah, 'S'
+    jmp error
+
+; http://wiki.osdev.org/Setting_Up_Long_Mode#x86_or_x86-64
+; Just assumes that cpuid is available (processor is released after 1993)
+check_long_mode:
+    mov eax, 0x80000000    ; Set the A-register to 0x80000000.
+    cpuid                  ; CPU identification.
+    cmp eax, 0x80000001    ; Compare the A-register with 0x80000001.
+    jb .no_long_mode       ; It is less, there is no long mode.
+    mov eax, 0x80000001    ; Set the A-register to 0x80000001.
+    cpuid                  ; CPU identification.
+    test edx, 1 << 29      ; Test if the LM-bit is set in the D-register.
+    jz .no_long_mode       ; They aren't, there is no long mode.
+    ret
+.no_long_mode:
+    ; error: no long mode: "!L"
+    mov al, '!'
+    mov ah, 'L'
+    jmp error
+
+; set up paging
+; http://os.phil-opp.com/entering-longmode.html#set-up-identity-paging
+; http://wiki.osdev.org/Paging
+; http://pages.cs.wisc.edu/~remzi/OSTEP/vm-paging.pdf
+; Identity map first 1GiB (0x200000 * 0x200)
+; using 2MiB pages
+set_up_page_tables:
+    ; map first P4 entry to P3 table
+    mov eax, page_table_p3
+    or eax, 0b11 ; present & writable
+    mov [page_table_p4], eax
+
+    ; map first P3 entry to P2 table
+    mov eax, page_table_p2
+    or eax, 0b11 ; present & writable
+    mov [page_table_p3], eax
+
+    ; map each P2 entry to a huge 2MiB page
+    mov ecx, 0         ; counter
+
+.map_page_table_p2_loop:
+    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
+    mov eax, 0x200000                   ; 2MiB
+    mul ecx                             ; page[ecx] start address
+    or eax, 0b10000011                  ; present & writable & huge
+    mov [page_table_p2 + ecx * 8], eax  ; map entry
+
+    inc ecx
+    cmp ecx, 0x200                  ; is the whole P2 table is mapped?
+    jne .map_page_table_p2_loop     ; next entry
+    ; done
+    ret
+
+; enable_paging
+; http://os.phil-opp.com/entering-longmode.html#enable-paging
+; http://wiki.osdev.org/Paging#Enabling
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, page_table_p4
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    ret
+
+
+
+; Prints `ERR: ` and the given 2-character error code to screen (TL) and hangs.
+; args: ax=(al,ah)=error_code (2 characters)
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov dword [0xb800a], 0x4f204f20
+    mov byte  [0xb800a], al
+    mov byte  [0xb800c], ah
+    hlt
+
+
+; Constant data
+
 gdtr32:
-    dw (gdt32_end - gdt32) + 1  ; size
-    dd gdt32                    ; offset
+    dw (gdt32.end - gdt32.begin) + 1    ; size
+    dd gdt32                            ; offset
 
 idtr32:
     dw 0
     dd 0
 
 gdt32:
+.begin:
     ; null entry
     dq 0
     ; code entry
@@ -107,7 +245,7 @@ gdt32:
     db 0b10010010   ; access byte - data
     db 0x4f         ; flags/(limit 16:19). flag is set to 32 bit protected mode
     db 0x00         ; base 24:31
-gdt32_end:
+.end:
 
 times (0x200 - 0x2)-($-$$) db 0
 db 0x55
--- src/boot/boot_stage1.asm
+++ src/boot/boot_stage1.asm
@@ -2,12 +2,6 @@
 ; STAGE 1
 
 
-; Page tables
-%define page_table_section_start 0x00020000
-%define page_table_p4 0x00020000
-%define page_table_p3 0x00021000
-%define page_table_p2 0x00022000
-%define page_table_section_end 0x00023000
 ; Kernel elf executable initial load point
 %define loadpoint 0x8000
 
@@ -17,19 +11,11 @@
 
 
 stage1:
-    ; SCREEN: top left: "01"
-    mov dword [0xb8000], 0x2f312f30
+    ; SCREEN: top left: "11"
+    mov dword [0xb8000], 0x2f312f31
 
-    ; "high-level" code block follows
-    call check_long_mode
-    call set_up_page_tables
-    call enable_paging
-    call set_up_SSE
-    ; end of "high-level" code block
-
-
-    ; SCREEN: top2 left: "2 "
-    mov dword [0xb8000 + 2*80], 0x2f202f32
+    ; SCREEN: top2 left: "12"
+    mov dword [0xb8000 + 2*80], 0x2f312f32
 
     mov ecx, 0xBEEF0001
 
@@ -108,109 +94,6 @@ stage1:
 
     ; jump into kernel entry (relocated to 0x00010000)
     jmp gdt64.code:0x00011000
-
-; Check for SSE and enable it.
-; http://os.phil-opp.com/set-up-rust.html#enabling-sse
-; http://wiki.osdev.org/SSE
-set_up_SSE:
-    ; check for SSE
-    mov eax, 0x1
-    cpuid
-    test edx, 1<<25
-    jz .SSE_missing
-
-    ; enable SSE
-    mov eax, cr0
-    and ax, 0xFFFB      ; clear coprocessor emulation CR0.EM
-    or ax, 0x2          ; set coprocessor monitoring  CR0.MP
-    mov cr0, eax
-    mov eax, cr4
-    or ax, 3 << 9       ; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
-    mov cr4, eax
-
-    ret
-.SSE_missing:
-    ; error: no SSE: "!S"
-    mov al, '!'
-    mov ah, 'S'
-    jmp error
-
-; http://wiki.osdev.org/Setting_Up_Long_Mode#x86_or_x86-64
-; Just assumes that cpuid is available (processor is released after 1993)
-check_long_mode:
-    mov eax, 0x80000000    ; Set the A-register to 0x80000000.
-    cpuid                  ; CPU identification.
-    cmp eax, 0x80000001    ; Compare the A-register with 0x80000001.
-    jb .no_long_mode       ; It is less, there is no long mode.
-    mov eax, 0x80000001    ; Set the A-register to 0x80000001.
-    cpuid                  ; CPU identification.
-    test edx, 1 << 29      ; Test if the LM-bit is set in the D-register.
-    jz .no_long_mode       ; They aren't, there is no long mode.
-    ret
-.no_long_mode:
-    ; error: no long mode: "!L"
-    mov al, '!'
-    mov ah, 'L'
-    jmp error
-
-; set up paging
-; http://os.phil-opp.com/entering-longmode.html#set-up-identity-paging
-; http://wiki.osdev.org/Paging
-; http://pages.cs.wisc.edu/~remzi/OSTEP/vm-paging.pdf
-; Identity map first 1GiB (0x200000 * 0x200)
-; using 2MiB pages
-set_up_page_tables:
-    ; map first P4 entry to P3 table
-    mov eax, page_table_p3
-    or eax, 0b11 ; present & writable
-    mov [page_table_p4], eax
-
-    ; map first P3 entry to P2 table
-    mov eax, page_table_p2
-    or eax, 0b11 ; present & writable
-    mov [page_table_p3], eax
-
-    ; map each P2 entry to a huge 2MiB page
-    mov ecx, 0         ; counter
-
-.map_page_table_p2_loop:
-    ; map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx
-    mov eax, 0x200000                   ; 2MiB
-    mul ecx                             ; page[ecx] start address
-    or eax, 0b10000011                  ; present & writable & huge
-    mov [page_table_p2 + ecx * 8], eax  ; map entry
-
-    inc ecx
-    cmp ecx, 0x200                  ; is the whole P2 table is mapped?
-    jne .map_page_table_p2_loop     ; next entry
-    ; done
-    ret
-
-; enable_paging
-; http://os.phil-opp.com/entering-longmode.html#enable-paging
-; http://wiki.osdev.org/Paging#Enabling
-enable_paging:
-    ; load P4 to cr3 register (cpu uses this to access the P4 table)
-    mov eax, page_table_p4
-    mov cr3, eax
-
-    ; enable PAE-flag in cr4 (Physical Address Extension)
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
-
-    ; set the long mode bit in the EFER MSR (model specific register)
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-    ; enable paging in the cr0 register
-    mov eax, cr0
-    or eax, 1 << 31
-    mov cr0, eax
-
-    ret
 
 
 ; Prints `ERR: ` and the given 2-character error code to screen (TL) and hangs.

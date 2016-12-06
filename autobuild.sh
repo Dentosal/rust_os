d3011c7c8cdf9daa2497c05c06c749e7b81c23e2
Dentosal <hannes.karppila@gmail.com>
2016-12-06 11:52:07 +0200

    Support bochs in autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 set -e
 
-# give -v to open in VirtualBox
 # give -u to run "vagrant up"
+# give -v to open in VirtualBox
+# give -b to open in Bochs
 # give -s to use "qemu -s" for gdb in port 1234
 # give -d to use additional debug options
-# give -c to only compile and not run
+# give -c to compile only
+# give -r to run only
 
-flag_vbox=0
-flag_debug=0
 flag_vagrant=0
+flag_vbox=0
+flag_bochs=0
 flag_qemu_s=0
+flag_debug=0
 flag_build_only=0
+flag_run_only=0
 
-while getopts 'abf:uvsdc' flag; do
+while getopts 'abf:uvbsdcr' flag; do
   case "${flag}" in
     u) flag_vagrant=1 ;;
     v) flag_vbox=1 ;;
+    b) flag_bochs=1 ;;
     s) flag_qemu_s=1 ;;
     d) flag_debug=1 ;;
     c) flag_build_only=1 ;;
+    r) flag_run_only=1 ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
-if [ $flag_vagrant -eq 1 ]
+if [ $flag_run_only -ne 1 ]
 then
-    vagrant up
+    if [ $flag_vagrant -eq 1 ]
+    then
+        vagrant up
+    fi
+    vagrant ssh -c "cd /vagrant/ && ./build.sh"
 fi
-
-vagrant ssh -c "cd /vagrant/ && ./build.sh"
 
 if [ $flag_build_only -ne 1 ]
 then
@@ -41,6 +49,9 @@ then
         else
             VBoxManage startvm "RustOS"
         fi
+    elif [ $flag_bochs -eq 1 ]
+    then
+        bochs -q -f bochs_config
     else
         if [ $flag_qemu_s -eq 1 ]
         then

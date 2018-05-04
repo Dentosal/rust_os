3560356d044718373f43c61255df9900c2b8b68f
Hannes Karppila <hannes.karppila@gmail.com>
2018-05-05 02:42:37 +0300

    Strip kernel binary after linking

--- build.sh
+++ build.sh
@@ -27,8 +27,7 @@ echo "* kernel"
 
 # compile kernel (with full optimizations)
 # RUST_TARGET_PATH=$(pwd) xargo build --target $TARGET --release
-# RUST_TARGET_PATH=$(pwd) xargo rustc --target $TARGET --release -- -C opt-level=s
-RUST_TARGET_PATH=$(pwd) xargo rustc --target $TARGET --release -- -C opt-level=z
+RUST_TARGET_PATH=$(pwd) xargo rustc --target $TARGET --release -- -C opt-level=s
 
 echo "* kernel assembly routines"
 mkdir -p build/asm_routines/
@@ -43,9 +42,9 @@ echo "Linking objects..."
 
 # link (use --print-gc-sections to debug)
 # ld -z max-page-size=0x1000  -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/$TARGET/release/librust_os.a build/asm_routines/*.o
-ld -z max-page-size=0x1000 --unresolved-symbols=ignore-all -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/$TARGET/release/librust_os.a build/asm_routines/*.o
-# ld -z max-page-size=0x1000 --gc-sections --print-gc-sections  -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/$TARGET/release/librust_os.a build/asm_routines/*.o
-# strip build/kernel.bin
+# ld -z max-page-size=0x1000 --unresolved-symbols=ignore-all -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/$TARGET/release/librust_os.a build/asm_routines/*.o
+ld -z max-page-size=0x1000 --gc-sections --print-gc-sections  -T buildsystem/linker.ld -o build/kernel.bin build/entry.o target/$TARGET/release/librust_os.a build/asm_routines/*.o
+strip build/kernel.bin
 
 echo "Cheking boundries..."
 

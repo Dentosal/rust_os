f12d5dda8698348736eb82d14d66256619ad40d8
Hannes Karppila <hannes.karppila@gmail.com>
2018-05-05 03:01:30 +0300

    Add kbd events

---
+++ src/keyboard/event.rs
@@ -0,0 +1,20 @@
+use super::key::Key;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum KeyboardEventType {
+    Press,
+    Release,
+    Repeat // ?
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct KeyboardEvent {
+    pub key: Key,
+    pub event_type: KeyboardEventType
+    // modifiers
+}
+impl KeyboardEvent {
+    pub fn new(key: Key, event_type: KeyboardEventType) -> KeyboardEvent {
+        KeyboardEvent { key, event_type }
+    }
+}

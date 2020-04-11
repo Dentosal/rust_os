fa814f4e223c307e13c105e0827559543f83f025
Hannes Karppila <hannes.karppila@gmail.com>
2020-04-11 18:56:47 +0300

    Add support for static attached branches

--- libs/libd7/src/attachment.rs
+++ libs/libd7/src/attachment.rs
@@ -1,8 +1,57 @@
+use alloc::prelude::v1::*;
 use d7abi::fs::FileDescriptor;
+use hashbrown::HashMap;
 
-pub use d7abi::fs::protocol::attachment::{Sender, Request, Response, FileOperation};
+pub use d7abi::fs::protocol::attachment::*;
+use d7abi::fs::protocol::ReadBranch;
 
 use crate::syscall::{self, SyscallResult};
+
+/// A safe wrapper for a static branch attachment point.
+/// Static branch contains a list of members,
+/// unlike normal branch that is rebuilt on every request.
+/// Clients cannot write to staticbranch at all.
+#[derive(Debug)]
+pub struct StaticBranch {
+    pub inner: Branch,
+    pub items: HashMap<String, FileDescriptor>,
+}
+impl StaticBranch {
+    pub fn new(path: &str) -> SyscallResult<Self> {
+        Ok(Self {
+            inner: Branch::new(path)?,
+            items: HashMap::new(),
+        })
+    }
+
+    /// Receive next request and reply
+    pub fn process_one(&self) -> SyscallResult<()> {
+        let r = self.inner.next_request()?;
+        match r.data {
+            FileOperation::Read(n) => {
+                // TODO: handle kernel requests differently
+                // let data = pinecone::to_vec(&ReadAttachmentBranch { items: self.items.clone() }).unwrap();
+
+                let data = pinecone::to_vec(&ReadBranch {
+                    items: self.items.keys().cloned().collect(),
+                })
+                .unwrap();
+
+                assert!(data.len() as u64 <= n, "TODO: implement output buffering");
+                self.inner.reply(r.response(data))?;
+            }
+            FileOperation::Close => {}
+            _ => panic!("Unsupported operation to a static branch ({:?})", r),
+        }
+        Ok(())
+    }
+
+    pub fn add_branch(&mut self, name: &str) -> SyscallResult<Branch> {
+        let new_branch = Branch::new_anonymous()?;
+        self.items.insert(name.to_owned(), new_branch.fd);
+        Ok(new_branch)
+    }
+}
 
 /// A safe wrapper for a branch attachment point
 #[derive(Debug)]
@@ -11,7 +60,28 @@ pub struct Branch {
 }
 impl Branch {
     pub fn new(path: &str) -> SyscallResult<Self> {
-        Ok(Self { fd: syscall::fs_attach(path, false)? })
+        Ok(Self {
+            fd: syscall::fs_attach(path, false)?,
+        })
+    }
+
+    pub fn new_anonymous() -> SyscallResult<Self> {
+        Self::new("")
+    }
+
+    /// Receive next request
+    pub fn next_request(&self) -> SyscallResult<Request> {
+        let mut buffer = [0u8; 32]; // TODO: is 32 always enough?
+        let count = syscall::fd_read(self.fd, &mut buffer)?;
+        Ok(pinecone::from_bytes(&buffer[..count]).unwrap())
+    }
+
+    /// Reply to a received request
+    pub fn reply(&self, response: Response) -> SyscallResult<()> {
+        let buffer = pinecone::to_vec(&response).unwrap();
+        let count = syscall::fd_write(self.fd, &buffer)?;
+        assert_eq!(buffer.len(), count, "TODO: Multipart writes");
+        Ok(())
     }
 }
 
@@ -22,7 +92,9 @@ pub struct Leaf {
 }
 impl Leaf {
     pub fn new(path: &str) -> SyscallResult<Self> {
-        Ok(Self { fd: syscall::fs_attach(path, true)? })
+        Ok(Self {
+            fd: syscall::fs_attach(path, true)?,
+        })
     }
 
     /// Receive next request

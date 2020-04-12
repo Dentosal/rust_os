0e251e03ead488673ff04786f7c92f066fa2c76f
Hannes Karppila <hannes.karppila@gmail.com>
2020-04-12 22:51:10 +0300

    Migrate ata pio driver module to new select! syntax

--- modules/driver_ata_pio/src/lib.rs
+++ modules/driver_ata_pio/src/lib.rs
@@ -7,9 +7,12 @@
 #[macro_use]
 extern crate alloc;
 
+#[macro_use]
+extern crate libd7;
+
 use alloc::prelude::v1::*;
 use hashbrown::HashMap;
-use libd7::{attachment::*, syscall};
+use libd7::{attachment::*, select, syscall};
 
 mod ata_pio;
 
@@ -19,7 +22,10 @@ fn main() -> ! {
 
     let controller = ata_pio::AtaPio::new();
 
-    let attachments: Vec<_> = (0..controller.drive_count())
+    let drive_count = controller.drive_count();
+    assert!(drive_count > 0, "No drives found");
+
+    let attachments: Vec<_> = (0..drive_count)
         .map(|drive_index| Leaf::new(&format!("/dev/ata_pio_{}", drive_index)).unwrap())
         .collect();
 
@@ -28,24 +34,27 @@ fn main() -> ! {
     let mut cursors: HashMap<Sender, u64> = HashMap::new();
 
     loop {
-        let avail_fd = syscall::fd_select(&attachment_fds, None).unwrap();
-        let index = attachment_fds
-            .iter()
-            .position(|fd| *fd == avail_fd)
-            .unwrap();
+        let index = select! {
+            any(attachment_fds) -> avail_fd => {
+                attachment_fds
+                    .iter()
+                    .position(|fd| *fd == avail_fd)
+                    .unwrap()
+            }
+        };
 
         let req = attachments[index].next_request().unwrap();
 
         let cur = *cursors.entry(req.sender).or_insert(0);
 
-        match req.data {
-            FileOperation::Read(byte_count) => {
+        match req.operation {
+            RequestFileOperation::Read(byte_count) => {
                 let sector_count = (byte_count / ata_pio::SECTOR_SIZE as u64) + 1;
                 assert!(sector_count <= (core::u8::MAX as u64));
-                let bytes = unsafe {
-                    controller.read_lba(index, cur, sector_count as u8)
-                };
-                attachments[index].reply(req.response(bytes)).unwrap();
+                let bytes = unsafe { controller.read_lba(index, cur, sector_count as u8) };
+                attachments[index]
+                    .reply(req.response(ResponseFileOperation::Read(bytes)))
+                    .unwrap();
             }
             other => unimplemented!("Unsupported request: {:?}", other),
         }

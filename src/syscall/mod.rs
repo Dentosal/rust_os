818b52156675237058dcd2585c91b0dd9bf22711
Hannes Karppila <hannes.karppila@gmail.com>
2020-04-15 18:00:58 +0300

    Properly trace-log anonymous files in fd_select

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -274,7 +274,7 @@ fn syscall(
                         let condition = fs.read_waiting_for(sched, fc)?;
 
                         let node_id = fs.resolve_fc(fc).unwrap();
-                        let path = fs.debug_node_id_to_path(node_id).unwrap();
+                        let path = fs.debug_node_id_to_path(node_id);
                         log::trace!("* {:?} ({:?}) cond = {:?}", fc, path, condition);
 
                         if condition == WaitFor::None {
